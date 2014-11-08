c604510bb66f9bfb2dff8cdcbf35f1e3b8366ff5
tpagithub <tpagithub@gmail.com>
2014-11-08 14:24:22 -0700

    Added a version in the comments to the Robot Template class.

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -17,10 +17,12 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
+ * documentation.If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
+ * Version: 0.1
  */
+
 public class RobotTemplate extends IterativeRobot {
     private TPAJoystick joystick;
     private TPARobotDrive robotDrive;
