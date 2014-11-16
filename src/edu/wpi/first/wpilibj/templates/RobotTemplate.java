3006cc860c4b502d203e5db6a4e18306c6562712
Samman <Samman@JavaPC>
2014-11-15 22:14:43 -0700

    Deleted a comment in the RobotTemplate class.

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -7,6 +7,7 @@
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
+/* Test */
 /*----------------------------------------------------------------------------*/
 
 package edu.wpi.first.wpilibj.templates;
